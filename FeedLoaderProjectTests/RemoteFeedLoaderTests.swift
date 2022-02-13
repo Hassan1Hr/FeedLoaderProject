be412967aa2b22df73f89f10e39461480c548711
Hassan Ramadan <hassanramadan4443@gmail.com>
2022-02-13 13:03:40 +0200

    test load func with init url

--- FeedLoaderProjectTests/RemoteFeedLoaderTests.swift
+++ FeedLoaderProjectTests/RemoteFeedLoaderTests.swift
@@ -41,6 +41,14 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL )
     }
 
-    
+    func test_load_requestDataFromURL()  {
+        
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL )
+    }
 
 }
