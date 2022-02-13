067843559902001d3c72f01d15a50fdb9b6be1d7
Hassan Ramadan <hassanramadan4443@gmail.com>
2022-02-13 13:09:31 +0200

    change HTTPClient to not be Singleton any more

--- FeedLoaderProjectTests/RemoteFeedLoaderTests.swift
+++ FeedLoaderProjectTests/RemoteFeedLoaderTests.swift
@@ -10,32 +10,34 @@ import XCTest
 
 class RemoteFeedLoader{
     func load(){
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClient{
     
-    static let shared = HTTPClient()
+    static var shared = HTTPClient()
     
-    private init() {}
+    func get (from url : URL) {}
+    
+}
+
+class HTTPClientSpy: HTTPClient{
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
     
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    
     func test_init_doesNotReqquestDataFromURL() throws {
         
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL )
@@ -43,7 +45,8 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_load_requestDataFromURL()  {
         
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         
         sut.load()
