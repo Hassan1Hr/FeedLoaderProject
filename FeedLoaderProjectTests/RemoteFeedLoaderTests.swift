f997826842b06c6084329324c70934adcbe0363c
Hassan Ramadan <hassanramadan4443@gmail.com>
2022-02-13 13:32:24 +0200

    refactor code to inject url and client move HTTOClientSpy to be inside our test not in production code

--- FeedLoaderProjectTests/RemoteFeedLoaderTests.swift
+++ FeedLoaderProjectTests/RemoteFeedLoaderTests.swift
@@ -11,11 +11,14 @@ import XCTest
 class RemoteFeedLoader{
     
     let client: HTTPClient
-    init(client: HTTPClient){
+    let url: URL
+    
+    init(url: URL, client: HTTPClient){
         self.client = client
+        self.url = url
     }
     func load(){
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -25,36 +28,43 @@ protocol HTTPClient{
     
 }
 
-class HTTPClientSpy: HTTPClient{
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-    
-    var requestedURL: URL?
-}
+
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     
     func test_init_doesNotReqquestDataFromURL() throws {
         
-        let client = HTTPClientSpy()
-       
-        _ = RemoteFeedLoader(client: client)
+        let (_,client) = makeSut()
         
         XCTAssertNil(client.requestedURL )
     }
 
     func test_load_requestDataFromURL()  {
-        
-        let client = HTTPClientSpy()
-       
-        let sut = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut,client) = makeSut(url: url)
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL )
     }
+    
+    // MARK: - Helpers
+    
+    
+    private func makeSut(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy){
+        let client = HTTPClientSpy()
+        
+        let sut = RemoteFeedLoader(url:url, client: client)
+        return(sut,client)
+    }
 
+    private class HTTPClientSpy: HTTPClient{
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+        
+        var requestedURL: URL?
+    }
 }
