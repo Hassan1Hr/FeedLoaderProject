af8bfc42ac11008cd56c9d9c1e81c7e224b063a4
Hassan Ramadan <hassanramadan4443@gmail.com>
2022-02-13 22:20:02 +0200

    go ahead to video 10

--- FeedLoaderProject/Feed Api/RemoteFeedLoader.swift
+++ FeedLoaderProject/Feed Api/RemoteFeedLoader.swift
@@ -7,10 +7,14 @@
 
 import Foundation
 
-public protocol HTTPClient{
-    
-    func get (from url : URL)
-    
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
+
+
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
 }
 
 
@@ -19,12 +23,27 @@ public class RemoteFeedLoader{
     private let client: HTTPClient
     private let url: URL
     
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    
     public init(url: URL, client: HTTPClient){
         self.client = client
         self.url = url
     }
     
-    public func load(){
-        client.get(from: url)
+    
+    public func load(completion: @escaping (Error) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+            case .success:
+                completion(.invalidData)
+            case .failure:
+                completion(.connectivity)
+            }
+        }
     }
+    
 }
